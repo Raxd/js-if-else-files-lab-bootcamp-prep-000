b8e5ce4593e09759f4a59d5a23da2a3559b42d8c
rafael sanchez <augusto4nc@gmail.com>
2017-11-05 14:18:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var name = "Joe"
-var height = 74
+var height = 70
 
 
 // Don't worry about this
 module.exports = { name, height
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> d79c2dbde3609460feeacf0cd800015e55fb7aca
